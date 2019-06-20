024ae13a785bdaf241c43fc08690a2c110d26892
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-20 12:01:23 +0800

    c++
    di er ci xiu gai

--- da fen/main.cpp
+++ da fen/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 struct student{
     char num[10];
-    char xueyuan[100];
+    char academy[100];
     char name[30];
     int score1;
     int score2;
@@ -11,111 +11,147 @@ struct student{
     int score5;
     int score6;
     int score7;
-};
-struct caipan{
-    int num;
-    int score;
+    double avg;
 };
 using namespace std;
 int main()
 {
-    int n;
-    cin>>n;
-    student b[n];
+    int number;
+    ifstream folder1("/Users/s20181102929/Desktop/xue shen ren shu.txt");
+    folder1>>number;
+    cout<<"ben ci can sai xuan shou gong you "<<number<<endl;
+    cout<<"ta meng de chen ji wo yi jing bang nin cu cun dao ben di wen jian jia!"<<endl;
+    student players[number];
     
-    ifstream a("/Users/s20181102929/Desktop/xue sheng xin xi.txt");
-    int i;
-    for(i=0;i<n;i++){
-        a>>b[i].num>>b[i].xueyuan>>b[i].name>>b[i].score1>>b[i].score2>>b[i].score3>>b[i].score4>>b[i].score5>>b[i].score6>>b[i].score7;
+    ifstream folder2("/Users/s20181102929/Desktop/xue sheng xin xi.txt");
+    int begin;
+    for(begin=0;begin<number;begin++){
+        folder2>>players[begin].num>>players[begin].academy>>players[begin].name>>players[begin].score1>>players[begin].score2>>players[begin].score3>>players[begin].score4>>players[begin].score5>>players[begin].score6>>players[begin].score7;
     }
-    for(i=0;i<n;i++){
-        int t=b[i].score1;
-        if(b[i].score2>t){
-            t=b[i].score2;
-        }
-        if(b[i].score3>t){
-            t=b[i].score3;
-        }
-        if(b[i].score4>t){
-            t=b[i].score4;
-        }
-        if(b[i].score5>t){
-            t=b[i].score5;
-        }
-        if(b[i].score6>t){
-            t=b[i].score6;
-        }
-        if(b[i].score7>t){
-            t=b[i].score7;
-        }
-        for(int j=0;j<n;j++){
-            if(b[j].score2==t){
-                b[j].score2=0;
+    for(begin=0;begin<number;begin++){
+        int instead[7];
+        instead[0]=players[begin].score1;
+        instead[1]=players[begin].score2;
+        instead[2]=players[begin].score3;
+        instead[3]=players[begin].score4;
+        instead[4]=players[begin].score5;
+        instead[5]=players[begin].score6;
+        instead[6]=players[begin].score7;
+        int middle=0;
+        for(int j=0;j<7;j++){
+            if(instead[j]>middle){
+                middle=instead[j];
             }
-            if(b[j].score3==t){
-                b[j].score3=0;
+        }
+        cout<<middle<<endl;
+        
+            if(players[begin].score1==middle){
+                players[begin].score1=0;
             }
-            if(b[j].score4==t){
-                b[j].score4=0;
+            if(players[begin].score2==middle){
+                players[begin].score2=0;
             }
-            if(b[j].score5==t){
-                b[j].score5=0;
+            if(players[begin].score3==middle){
+                players[begin].score3=0;
             }
-            if(b[j].score6==t){
-                b[j].score6=0;
+            if(players[begin].score4==middle){
+                players[begin].score4=0;
             }
-            if(b[j].score7==t){
-                b[j].score7=0;
+            if(players[begin].score5==middle){
+                players[begin].score5=0;
+            }
+            if(players[begin].score6==middle){
+                players[begin].score6=0;
+            }
+            if(players[begin].score7==middle){
+                players[begin].score7=0;
+            }
+        }
+    for(begin=0;begin<number;begin++){
+        int instead[7];
+        instead[0]=players[begin].score1;
+        instead[1]=players[begin].score2;
+        instead[2]=players[begin].score3;
+        instead[3]=players[begin].score4;
+        instead[4]=players[begin].score5;
+        instead[5]=players[begin].score6;
+        instead[6]=players[begin].score7;
+        int middle;
+        for(int j=0;j<number;j++){
+            if(players[j].score1!=0){
+                middle=players[j].score1;
+            }
+            if(players[j].score2!=0){
+                middle=players[j].score2;
+            }
+            if(players[j].score3!=0){
+               middle=players[j].score3;
+            }
+            if(players[j].score4!=0){
+                middle=players[j].score4;
+            }
+            if(players[j].score5!=0){
+                middle=players[j].score5;
+            }
+            if(players[j].score6!=0){
+                middle=players[j].score6;
+            }
+            if(players[j].score7!=0){
+                middle=players[j].score7;
+            }
+        }
+        for(int j=0;j<7;j++){
+            if(instead[j]!=0)
+                if(instead[j]<middle){
+                    middle=instead[j];
+                }
+        }
+        cout<<middle<<endl;
+        if(players[begin].score1==middle){
+            players[begin].score1=0;
+        }
+        if(players[begin].score2==middle){
+            players[begin].score2=0;
+        }
+        if(players[begin].score3==middle){
+           players[begin].score3=0;
+        }
+        if(players[begin].score4==middle){
+            players[begin].score4=0;
+        }
+        if(players[begin].score5==middle){
+            players[begin].score5=0;
+        }
+        if(players[begin].score6==middle){
+            players[begin].score6=0;
+        }
+        if(players[begin].score7==middle){
+            players[begin].score7=0;
+        }
+    }
+    for(begin=0;begin<number;begin++){
+        double h=0;
+        h=players[begin].score1+players[begin].score2+players[begin].score3+players[begin].score4+players[begin].score5+players[begin].score6+players[begin].score7;
+        h=1.0*h/5;
+        players[begin].avg=h;
+    }
+    student pm;
+    for(begin=0;begin<number-1;begin++){
+        for(int j=0;j<number-1;j++){
+            if(players[j].avg<players[j+1].avg){
+                pm=players[j];
+               players[j]=players[j+1];
+                players[j+1]=pm;
             }
         }
     }
-    for(i=0;i<n;i++){
-        int t=b[i].score1;
-        if(b[i].score3<t){
-            t=b[i].score3;
+    ofstream folder3("/Users/s20181102929/Desktop/cucunxinxi.txt");
+    if(folder3.is_open()){
+        for(begin=0;begin<number;begin++){
+            folder3<<"zui hou de fen:"<<players[begin].avg<<" "<<"zui hou pai ming :"<<begin+1<<endl;
+            folder3<<"he is:"<<players[begin].num<<" "<<players[begin].academy<<" "<<players[begin].name<<endl;
         }
-        if(b[i].score4<t){
-            t=b[i].score4;
-        }
-        if(b[i].score5<t){
-            t=b[i].score5;
-        }
-        if(b[i].score6<t){
-            t=b[i].score6;
-        }
-        if(b[i].score7<t){
-            t=b[i].score7;
-        }
-        for(int j=0;j<n;j++){
-            if(b[j].score1==t){
-                b[j].score1=0;
-            }
-            if(b[j].score2==t){
-                b[j].score2=0;
-            }
-            if(b[j].score3==t){
-                b[j].score3=0;
-            }
-            if(b[j].score4==t){
-                b[j].score4=0;
-            }
-            if(b[j].score5==t){
-                b[j].score5=0;
-            }
-            if(b[j].score6==t){
-                b[j].score6=0;
-            }
-            if(b[j].score7==t){
-                b[j].score7=0;
-            }
-        }
-    }
-    ofstream c("/Users/s20181102929/Desktop/cucunxinxi.txt");
-    if(c.is_open()){
-        for(i=0;i<n;i++){
-            c<<b[i].num<<" "<<b[i].xueyuan<<" "<<b[i].name<<" "<<b[i].score1<<" "<<b[i].score2<<" "<<b[i].score3<<" "<<b[i].score4<<" "<<b[i].score5<<" "<<b[i].score6<<" "<<b[i].score7<<endl;
-        }
-        c.close();
+        folder3.close();
     }
     return 0;
 }
