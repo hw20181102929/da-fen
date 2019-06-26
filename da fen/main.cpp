1b3ea33ddb90953fd810d9ebc25bc313cb285155
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-06-26 18:43:38 +0800

    c++
    zai ci gen gai

--- da fen/main.cpp
+++ da fen/main.cpp
@@ -19,9 +19,10 @@ int main()
     int number;
     ifstream folder1("/Users/s20181102929/Desktop/比赛打分表/参赛人数.txt");
     folder1>>number;
-    cout<<"本次参赛的选手共有 "<<number<<"名"<<"!"<<endl;
-    cout<<"他们的比赛成绩我已经放到了本地文件夹里！"<<endl;
-    cout<<"请注意查收！"<<endl;
+    cout<<"本次参赛的选手共有 "<<number<<"名"<<"!"<<endl<<endl;
+    cout<<"他们的详细比赛成绩我已经放到了本地比赛打分文件夹下！"<<endl<<endl;
+    cout<<"如果您想浏览比赛结果，请到本地文件夹下查看"<<endl<<endl;
+    cout<<"下面将为你呈现本次比赛中的一些基本信息："<<endl<<endl;
     student players[number];
     ifstream folder2("/Users/s20181102929/Desktop/比赛打分表/学生的基本信息.txt");
     int begin;
@@ -32,11 +33,12 @@ int main()
     for(begin=0;begin<number;begin++){
         folder4>>players[begin].score1>>players[begin].score2>>players[begin].score3>>players[begin].score4>>players[begin].score5>>players[begin].score6>>players[begin].score7;
     }
-    cout<<"他们获得的成绩分别为："<<endl;
+    cout<<"各位裁判的对选手的打分如下： "<<endl;
     for(begin=0;begin<number;begin++){
         cout<<begin+1<<"号选手的得分为：";
-        cout<<players[begin].score1<<" " <<players[begin].score2<<" "<<players[begin].score3<<" "<<players[begin].score4<<" "<<players[begin].score5<<" "<<players[begin].score6<<" "<<players[begin].score7<<endl;
+        cout<<players[begin].score1<<" " <<players[begin].score2<<" "<<players[begin].score3<<" "<<players[begin].score4<<" "<<players[begin].score5<<" "<<players[begin].score6<<" "<<players[begin].score7<<endl<<endl;
     }
+    
     for(begin=0;begin<number;begin++){
         int instead[7];
         instead[0]=players[begin].score1;
@@ -52,7 +54,6 @@ int main()
                 middle=instead[j];
             }
         }
-       // cout<<middle<<endl;
             if(players[begin].score1==middle){
                 players[begin].score1=0;
             }
@@ -114,7 +115,6 @@ int main()
                     middle=instead[middlechange];
                 }
         }
-        //cout<<middle<<endl;
         if(players[begin].score1==middle){
             players[begin].score1=0;
         }
@@ -139,7 +139,7 @@ int main()
     }
     for(begin=0;begin<number;begin++){
         double h=0;
-        h=players[begin].score1+players[begin].score2+players[begin].score3+players[begin].score4+players[begin].score5+players[begin].score6+players[begin].score7;
+     h=players[begin].score1+players[begin].score2+players[begin].score3+players[begin].score4+players[begin].score5+players[begin].score6+players[begin].score7;
         h=1.0*h/5;
         players[begin].avg=h;
     }
@@ -156,8 +156,9 @@ int main()
    
     ofstream folder3("/Users/s20181102929/Desktop/比赛打分表/比赛信息.txt");
     if(folder3.is_open()){
-         folder3<<"本次的比赛结果如下："<<endl;
-        folder3<<"下面由我来给大家揭晓："<<endl;
+        folder3<<"在经过精彩而又激烈的竞争后，本次比赛也拉下帷幕"<<endl<<endl<<"各位选手也在本次比赛中取得了优异的成绩！！！！"<<endl<<endl<<endl;
+         folder3<<"本次的比赛结果如下："<<endl<<endl;
+        folder3<<"下面由我来给大家揭晓："<<endl<<endl<<endl<<endl;
         for(begin=0;begin<number;begin++){
             folder3<<"获得本次比赛第"<<begin+1<<"名的是："<<endl;
             folder3<<"他是来自:"<<players[begin].academy<<" "<<"学号为："<<players[begin].num<<"的"<<" "<<" "<<players[begin].name<<"同学"<<endl;
@@ -168,7 +169,7 @@ int main()
             else{
                 folder3<<"希望他能在下次的比赛中取得更加优异的成绩！"<<endl;
             }
-            folder3<<"感谢他的参与！"<<endl;
+            folder3<<"感谢他的参与！"<<endl<<endl<<endl;
             folder3<<endl;
         }
         folder3.close();
