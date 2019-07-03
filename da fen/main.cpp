24a2b90e51b40d5ce5114963be7ac49a5b1ef285
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-07-03 19:03:50 +0800

    Update main.cpp
    7 3

--- da fen/main.cpp
+++ da fen/main.cpp
@@ -3,28 +3,28 @@
 #include <algorithm>
 
 struct student{
-    char num[10];
-    char academy[100];
-    char name[30];
-    int score1;
-    int score2;
-    int score3;
-    int score4;
-    int score5;
-    int score6;
-    int score7;
-    double average;
+    char num[10];//学号
+    char academy[100];//学院
+    char name[30];//姓名
+    int score1;//裁判一的打分
+    int score2;//裁判二的打分
+    int score3;//裁判三的打分
+    int score4;//裁判四的打分
+    int score5;//裁判五的打分
+    int score6;//裁判六的打分
+    int score7;//c裁判七的打分
+    double average;//学生的平均分
 };
 bool cmp(const student &stu1,const student &stu2){
     return stu1.average>stu2.average;
-}
+}//算法sort的结构体排序；
 using namespace std;
 void print(){
     int begin;
     for(begin=0;begin<46;begin++){
         cout<<"*";
     }
-}
+}//打印函数；
 void print1(){
     int start;
    for(start=0;start<46;start++){
@@ -33,7 +33,7 @@ void print1(){
     cout<<endl;
     for(start=0;start<46;start++)
         cout<<"*";
-}
+}//打印函数；
 int main()
 {
     int number; int begin;
@@ -48,7 +48,7 @@ int main()
     cout<<endl;
     cout<<"他们的详细比赛成绩我已经放到了本地比赛打分文件夹下！"<<endl<<endl;
     cout<<"如果您想浏览比赛结果，请到本地文件夹下查看"<<endl<<endl;
-    cout<<"下面将为你呈现本次比赛中的一些基本信息："<<endl;
+    cout<<"下面将为你呈现本次比赛中的一些基本信息："<<endl;//在输出框输出基本信息
     for(begin=0;begin<46;begin++){
         cout<<"*";
     }
@@ -57,16 +57,16 @@ int main()
     ifstream folder2("/Users/s20181102929/Desktop/比赛打分表/学生的基本信息.txt");
     for(begin=0;begin<number;begin++){
         folder2>>players[begin].num>>players[begin].academy>>players[begin].name;
-    }
+    }//从文本输入每个学生的基本信息；
     ifstream folder4("/Users/s20181102929/Desktop/比赛打分表/裁判打分.txt");
     for(begin=0;begin<number;begin++){
  folder4>>players[begin].score1>>players[begin].score2>>players[begin].score3>>players[begin].score4>>players[begin].score5>>players[begin].score6>>players[begin].score7;
-    }
+    }//从文本输入每个学生的分数！；
     cout<<"各位裁判的对选手的打分如下： "<<endl;
     for(begin=0;begin<number;begin++){
         cout<<begin+1<<"号选手的得分为：";
         cout<<players[begin].score1<<" " <<players[begin].score2<<" "<<players[begin].score3<<" "<<players[begin].score4<<" "<<players[begin].score5<<" "<<players[begin].score6<<" "<<players[begin].score7<<endl<<endl;
-    }
+    }//在屏幕输出每个裁判对学生的打分！；
     print();
     cout<<endl;
     for(begin=0;begin<number;begin++){
@@ -138,7 +138,7 @@ int main()
             if(players[middlechange].score7!=0){
                 middle=players[middlechange].score7;
             }
-        }
+        }//找最大值；
         for(int middlechange=0;middlechange<7;middlechange++){
             if(instead[middlechange]!=0)
                 if(instead[middlechange]<middle){
@@ -166,39 +166,29 @@ int main()
         if(players[begin].score7==middle){
             players[begin].score7=0;
         }
-    }
+    }//找最小值；
     for(begin=0;begin<number;begin++){
         double sumscore=0;
         sumscore=players[begin].score1+players[begin].score2+players[begin].score3+players[begin].score4+players[begin].score5+players[begin].score6+players[begin].score7;
         sumscore=1.0*sumscore/5;
         players[begin].average=sumscore;
-    }
-    sort(players,players+number,cmp);
-    
+    }//求每个学生的平均成绩；
+    sort(players,players+number,cmp);//根据平均分进行排序；
     ofstream folder3("/Users/s20181102929/Desktop/比赛打分表/比赛信息.txt");
-    if(folder3.is_open()){
+    if(folder3.is_open()){//向文件夹里输入基本信息；
         for(int start=0;start<56;start++){
             folder3<<"*";
         }
         folder3<<endl;
-        folder3<<"在经过精彩而又激烈的竞争后，本次比赛也拉下帷幕"<<endl<<endl<<"各位选手也在本次比赛中取得了优异的成绩！！！！"<<endl<<endl<<endl;
         folder3<<"本次的比赛结果如下："<<endl<<endl;
-        folder3<<"下面由我来给大家揭晓："<<endl<<endl;
         for(begin=0;begin<56;begin++){
             folder3<<"*";
         }
         folder3<<endl<<endl;
         for(begin=0;begin<number;begin++){
             folder3<<"获得本次比赛第"<<begin+1<<"名的是："<<endl;
-            folder3<<"他是来自:"<<"     "<<players[begin].academy<<"       "<<"学号为："<<"  "<<players[begin].num<<"     "<<" 的"<<players[begin].name<<"同学"<<endl;
-            folder3<<"去掉一个最高分去掉一个最低分他的最后得分为："<<endl<<endl<<players[begin].average<<endl<<endl;
-            if(begin==0){
-                folder3<<"希望他能再接再厉！祝贺他本次取得好成绩！"<<endl;
-            }
-            else{
-                folder3<<"希望他能在下次的比赛中取得更加优异的成绩！"<<endl;
-            }
-            folder3<<"感谢他的参与！"<<endl<<endl<<endl;
+            folder3<<"他是来自:"<<"     "<<players[begin].academy<<"       "<<"学号为："<<"  "<<players[begin].num<<"     "<<" 的"<<" "<<players[begin].name<<" "<<"同学"<<endl;
+            folder3<<"他的最后得分为："<<endl<<endl<<players[begin].average<<endl<<endl;
             for(int start=0;start<56;start++){
                 folder3<<"*";
             }
