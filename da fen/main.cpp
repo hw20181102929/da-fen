49306119b5f5975f138b7046c766c9511e6b183d
hw20181102929 <51589984+hw20181102929@users.noreply.github.com>
2019-07-03 19:53:36 +0800

    Update main.cpp
    ZUI HOU

--- da fen/main.cpp
+++ da fen/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#define NUMBER 10
 struct student{
     char num[10];
     char academy[100];
@@ -34,12 +35,10 @@ void print1(){
 }
 int main()
 {
-    int number; int i;
-    ifstream folder1("/Users/s20181102929/Desktop/比赛打分表/参赛人数.txt");
-    folder1>>number;
+    int i;
     print();
     cout<<endl;
-    cout<<"本次参赛的选手共有 "<<number<<"名"<<"!"<<endl;
+    cout<<"本次参赛的选手共有 "<<NUMBER<<"名"<<"!"<<endl;
     for(i=0;i<46;i++){
         cout<<"*";
     }
@@ -51,23 +50,23 @@ int main()
         cout<<"*";
     }
     cout<<endl<<endl;
-    student players[number];
+    student players[NUMBER];
     ifstream folder2("/Users/s20181102929/Desktop/比赛打分表/学生的基本信息.txt");
-    for(i=0;i<number;i++){
+    for(i=0;i<NUMBER;i++){
         folder2>>players[i].num>>players[i].academy>>players[i].name;
     }
     ifstream folder4("/Users/s20181102929/Desktop/比赛打分表/裁判打分.txt");
-    for(i=0;i<number;i++){
+    for(i=0;i<NUMBER;i++){
  folder4>>players[i].score1>>players[i].score2>>players[i].score3>>players[i].score4>>players[i].score5>>players[i].score6>>players[i].score7;
     }
     cout<<"各位裁判的对选手的打分如下： "<<endl;
-    for(i=0;i<number;i++){
+    for(i=0;i<NUMBER;i++){
         cout<<i+1<<"号选手的得分为：";
         cout<<players[i].score1<<" " <<players[i].score2<<" "<<players[i].score3<<" "<<players[i].score4<<" "<<players[i].score5<<" "<<players[i].score6<<" "<<players[i].score7<<endl<<endl;
     }
     print();
     cout<<endl;
-    for(i=0;i<number;i++){
+    for(i=0;i<NUMBER;i++){
         int instead[7];
         instead[0]=players[i].score1;
         instead[1]=players[i].score2;
@@ -104,7 +103,7 @@ int main()
             players[i].score7=0;
         }
     }
-    for(i=0;i<number;i++){
+    for(i=0;i<NUMBER;i++){
         int instead[7];
         instead[0]=players[i].score1;
         instead[1]=players[i].score2;
@@ -114,7 +113,7 @@ int main()
         instead[5]=players[i].score6;
         instead[6]=players[i].score7;
         int middle;
-        for(int middlechange=0;middlechange<number;middlechange++){
+        for(int middlechange=0;middlechange<NUMBER;middlechange++){
             if(players[middlechange].score1!=0){
                 middle=players[middlechange].score1;
             }
@@ -165,13 +164,13 @@ int main()
             players[i].score7=0;
         }
     }
-    for(i=0;i<number;i++){
+    for(i=0;i<NUMBER;i++){
         double sumscore=0;
         sumscore=players[i].score1+players[i].score2+players[i].score3+players[i].score4+players[i].score5+players[i].score6+players[i].score7;
         sumscore=1.0*sumscore/5;
         players[i].average=sumscore;
     }
-    sort(players,players+number,cmp);
+    sort(players,players+NUMBER,cmp);
     ofstream folder3("/Users/s20181102929/Desktop/比赛打分表/比赛信息.txt");
     if(folder3.is_open()){
         for(int start=0;start<56;start++){
@@ -183,7 +182,7 @@ int main()
             folder3<<"*";
         }
         folder3<<endl<<endl;
-        for(i=0;i<number;i++){
+        for(i=0;i<NUMBER;i++){
             folder3<<"获得本次比赛第"<<i+1<<"名的是："<<endl;
             folder3<<"他是来自:"<<"     "<<players[i].academy<<"       "<<"学号为："<<"  "<<players[i].num<<"     "<<" 的"<<" "<<players[i].name<<" "<<"同学"<<endl;
             folder3<<"他的最后得分为："<<endl<<endl<<players[i].average<<endl<<endl;
